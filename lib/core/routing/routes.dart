8ade7fe20573a25fd5c9d01ef11594947bc1af95
Mariam Emad <93050116+Mariam-Emad127@users.noreply.github.com>
2025-03-17 21:04:09 +0200

    router2

---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,11 @@
+class Routes{
+static const String login="/login"; 
+static const String signin="/signin"; 
+static const String HomeScreen="/HomeScreen"; 
+static const String onboarding="/onboarding"; 
+static const String addingPost="/adding_post"; 
+static const String social_screen="/social_screen"; 
+
+
+ 
+}
