2550ab754193d5c8daec63ff220f14d9966a8ca0
Mariam Emad <93050116+Mariam-Emad127@users.noreply.github.com>
2025-07-09 16:10:41 +0300

    Update string.dart

--- lib/core/utils/string.dart
+++ lib/core/utils/string.dart
@@ -9,10 +9,5 @@ static String aPPkey=
 "os_v2_app_vmkc4pkh4bbglniucohphjqmfftncoq2t2cemv5qxkzuvxvhiuufg6fupuy43x4mjfvb66ftj7gbyjxa5vdemhduzrbjxqzm4wtfskq";
 //"tncoq2t2cemv5qxkzuvxvhiuu";
  static const String boxname= 'NoteBox2';
-
-//"os_v2_app_vmkc4pkh4bbglniucohphjqmfgrjga3cqesujif54zjngff5fajymj7jtjhz2wty6n7yh5iu3hs5wgasvlbfirns6ayfphkpw3aihoq";
-//"rjga3cqesujif54zjngff5faj";
-// "fr7auugiuuwofep35enlaorcw";
- //"os_v2_app_vmkc4pkh4bbglniucohphjqmfefr7auugiuuwofep35enlaorcwi5vo56qh5xzp4yt5wlgp2s7dm3dhpzhhluvzbbzvqq3h4i4du3kq";
  
 }
